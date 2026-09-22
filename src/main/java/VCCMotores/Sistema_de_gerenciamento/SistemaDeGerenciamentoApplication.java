ed1933cc7ea230866f773a701bbe06206596d831
Daniel Menezes <dfm.daniel.menezes@gmail.com>
2026-09-21 21:21:48 -0300

    Primeiro commit

---
+++ src/main/java/VCCMotores/Sistema_de_gerenciamento/SistemaDeGerenciamentoApplication.java
@@ -0,0 +1,13 @@
+package VCCMotores.Sistema_de_gerenciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGerenciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGerenciamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/VCCMotores/Sistema_de_gerenciamento/SistemaDeGerenciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package VCCMotores.Sistema_de_gerenciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGerenciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
